84d76d5c67f1c6b252414abf7be7fbd737a89c95
Krishna A S <krishnasaye23@gmail.com>
2025-03-21 07:34:53 -0400

    Script changes

--- terraform/install-k8s.sh
+++ terraform/install-k8s.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-set -e
+set -e  # Exit immediately if a command fails
 
-# Update and install required packages
-sudo apt update -y
-sudo apt install -y curl apt-transport-https ca-certificates software-properties-common
+# Update and install required packages (Amazon Linux uses yum/dnf, not apt)
+sudo yum update -y
+sudo yum install -y curl tar unzip ca-certificates
 
 # Install Docker
-sudo apt install -y docker.io
+sudo yum install -y docker
 sudo systemctl enable docker
 sudo systemctl start docker
 
-# Install kubectl
-sudo curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+# Install kubectl (Use Amazon Linux-compatible URL)
+sudo curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
@@ -21,12 +21,12 @@ sudo chmod +x kind
 sudo mv kind /usr/local/bin/kind
 
 # Create kind cluster
-sudo kind create cluster
+sudo /usr/local/bin/kind create cluster
 
-# Add user to Docker group (Optional)
-sudo usermod -aG docker ubuntu
+# Add user to Docker group (Amazon Linux uses 'ec2-user' instead of 'ubuntu')
+sudo usermod -aG docker ec2-user
 
 # Output versions
 docker --version
-kubectl --version
+kubectl version --client
 kind version
