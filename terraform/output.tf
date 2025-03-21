791de4d8af52fd6e32eb57f5806ea60e64b92644
Krishna A S <krishnasaye23@gmail.com>
2025-03-21 05:39:56 -0400

    Error fix

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,4 @@
 output "ec2_public_ip" {
   description = "Public IP address of the Kubernetes EC2 instance"
-  value       = aws_instance.k8s_node.public_ip
+  value       = aws_instance.project_instance.public_ip
 }
