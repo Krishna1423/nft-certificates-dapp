1630b5dffb09a227de06ad1f44b6d451ff0402d0
Krishna A S <krishnasaye23@gmail.com>
2025-03-21 17:43:39 -0400

    RBAC files added

--- terraform/install-k8s.sh
+++ terraform/install-k8s.sh
@@ -21,14 +21,12 @@ sudo usermod -aG docker ec2-user  # Add ec2-user to Docker group
 
 # Install kubectl (Amazon Linux 2023)
 curl -LO https://dl.k8s.io/release/v1.32.0/bin/linux/amd64/kubectl
-chmod +x kubectl
-sudo mv kubectl /usr/local/bin/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # Install Kind
 curl -Lo kind https://kind.sigs.k8s.io/dl/latest/kind-linux-amd64
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-#chmod +x kind
-#sudo mv kind /usr/local/bin/kind
+chmod +x kind
+sudo mv kind /usr/local/bin/kind
 
 # Restart Docker (ensure it's running)
 sudo systemctl restart docker
